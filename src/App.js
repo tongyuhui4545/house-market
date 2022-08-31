8511b5f8f70bbf7bd909130c575dfc577b5210ee
tongyuhui4545 <tongyuhui4545@gmail.com>
2022-08-31 09:15:43 +0800

    Contact Page done
    The listing and contact page are finished.
    
    Now the user can click the item under either category
    
    and click the item to get to the contact page.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import Category from "./pages/Category";
 import Profile from "./pages/Profile";
+import Listing from "./pages/Listing";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Contact from "./pages/Contact";
 import CreateListing from "./pages/CreateListing";
 import ForgotPassword from "./pages/ForgotPassword";
 
@@ -20,6 +22,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/contact/:landlordId" element={<Contact />} />
           <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
