f8efc215d416285a07b841ecdf40d4282f5a64ae
tongyuhui4545 <tongyuhui4545@gmail.com>
2022-08-31 09:56:46 +0800

    Map Added
    Now the leaflet Map is added.
    
    User can view the location on the map when clicking in a certain item of listing.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
@@ -97,7 +98,24 @@ const Listing = () => {
           </ul>
 
           <p className="listingLocationTitle">Location</p>
-          {/* MAP */}
+          <div className="leafletContainer">
+            <MapContainer
+              style={{ height: "100%", width: "100%" }}
+              center={[listing.geolocation.lat, listing.geolocation.lng]}
+              zoom={13}
+              scrollWheelZoom={false}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker
+                position={[listing.geolocation.lat, listing.geolocation.lng]}
+              >
+                <Popup>{listing.location}</Popup>
+              </Marker>
+            </MapContainer>
+          </div>
           {auth.currentUser?.uid !== listing.userRef && (
             <Link
               to={`/contact/${listing.userRef}?listingName=${listing.name}`}
